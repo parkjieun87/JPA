b409834a0a080e39ef10cb1e073e25dc02ccea41
parkjieun87 <dksxlzhd0807@naver.com>
2024-02-06 13:06:43 +0900

    Feat : "JPA 프로젝트 생성"

---
+++ jpashop/src/main/java/jpaproject/jpashop/JpashopApplication.java
@@ -0,0 +1,16 @@
+package jpaproject.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jpaproject.jpashop.controller.Hello;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
