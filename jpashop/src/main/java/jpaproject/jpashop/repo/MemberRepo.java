b440e32e5ff45259b1fc2f290c618addd0c075c2
parkjieun87 <dksxlzhd0807@naver.com>
2024-02-14 10:54:52 +0900

    Feat:"MemberRepo 생성"

--- jpashop/src/main/java/jpaproject/jpashop/repo/MemberRepo.java
+++ jpashop/src/main/java/jpaproject/jpashop/repo/MemberRepo.java
@@ -1,5 +1,7 @@
 package jpaproject.jpashop.repo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import jakarta.persistence.EntityManager;
@@ -8,16 +10,29 @@ import jpaproject.jpashop.model.Member;
 
 @Repository
 public class MemberRepo {
-
+	
 	@PersistenceContext
 	private EntityManager em;
-	
-	public Long save(Member member) {
+
+	//저장
+	public void save(Member member) {
 		em.persist(member);
-		return member.getId();
 	}
-	
-	public Member find(Long id) {
+
+	//id로 조회
+	public Member findOne(Long id) {
 		return em.find(Member.class, id);
+	}
+
+	//리스트조회
+	public List<Member> findAll(){
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	//이름으로 조회
+	public List<Member> findName(String name){
+		return em.createQuery("select m from Member m where m.name = :name",Member.class)
+				.setParameter("name", name).getResultList(); 
 	}
 }
