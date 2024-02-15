83d8d85e57bb27b7109370a98b51505bab497507
parkjieun87 <dksxlzhd0807@naver.com>
2024-02-15 14:20:21 +0900

    Feat:"검색model 생성"

---
+++ jpashop/src/main/java/jpaproject/jpashop/model/OrderSearch.java
@@ -0,0 +1,11 @@
+package jpaproject.jpashop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OrderSearch {
+	 private String memberName;
+	 private OrderStatus orderStatus;
+}
