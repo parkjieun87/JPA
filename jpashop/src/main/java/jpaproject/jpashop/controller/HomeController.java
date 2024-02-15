22de8d5075f212517ef1d49e2caa7e50d3387e11
parkjieun87 <dksxlzhd0807@naver.com>
2024-02-15 15:17:40 +0900

    Feat:"html 생성"

---
+++ jpashop/src/main/java/jpaproject/jpashop/controller/HomeController.java
@@ -0,0 +1,19 @@
+package jpaproject.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+	@RequestMapping("/")
+	 public String home() {
+		log.info("home controller");
+		return "home";
+	}
+}
